d92121af42e0d2487f8ddbcb3ef543c153e7308c
dharumaraj <dharumaraj30092000@gmail.com>
2023-03-06 11:41:03 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var string="dharumaraj";
+var count=0;
+
+for(let i=0;i<string.length;i++){
+	if(string[i]==="a"||string[i]==="e"||string[i]==="i"||string[i]==="o"||string[i]==="u"){
+		count++;
+	}
+}
+console.log(count+" "+"vowels");
